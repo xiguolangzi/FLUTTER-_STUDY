ff85be6b9f214463fdbbe16e63ebe603c3fe4ece
BG <hubiao19910129@gmail.com>
2023-04-06 17:09:50 +0200

    日历组件

--- lib/10_form/05_calendar.dart
+++ lib/10_form/05_calendar.dart
@@ -33,6 +33,114 @@ class CalendarDemo extends StatefulWidget {
 }
 
 class _CalendarDemoState extends State<CalendarDemo> {
+  // 1.声明时间返回值
+  DateTime _date = DateTime.now();
+  TimeOfDay _times = TimeOfDay.now();
+  String _apm =
+      TimeOfDay.now().period.toString() == "DayPeriod.am" ? "上午" : "下午";
+  String _showTimes = TimeOfDay.now().period.toString() == "DayPeriod.am"
+      ? "上午 - "
+      : "下午 - ${TimeOfDay.now().toString().substring(10, 15)}";
+
+  // 2.1 获取时间返回值 - 方式一
+  _showDatepickeDayInput() async {
+    // 异步函数
+    showDatePicker(
+      // 日历基础设置
+      context: context,
+      initialDatePickerMode: DatePickerMode.day,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015, 6),
+      lastDate: DateTime(2025, 6),
+      currentDate: DateTime.now(),
+
+      // 弹出模式 - 日历模式
+      initialEntryMode: DatePickerEntryMode.input,
+
+      // 日历文字
+      helpText: "年份选择器",
+      cancelText: "取消",
+      confirmText: "确认",
+
+      // 日历提示信息
+      errorFormatText: "输入格式错误",
+      errorInvalidText: "输入时间超出范围",
+      fieldLabelText: "请输入时间",
+      fieldHintText: "时间格式 月/日/年",
+
+      // 异步函数返回值
+    ).then((value) {
+      setState(() {
+        // 日历取消返回 null 处理
+        if (value != null) {
+          _date = value;
+        }
+      });
+    });
+  }
+
+  //  2.2 获取时间返回值 - 方式二
+  _showDatePickerCalendar() async {
+    var _picker = await showDatePicker(
+      context: context,
+      initialDatePickerMode: DatePickerMode.day,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015, 6),
+      lastDate: DateTime(2025, 6),
+
+      // 弹出模式 - 日历模式
+      initialEntryMode: DatePickerEntryMode.calendar,
+
+      // 当前时间
+      currentDate: DateTime.now(),
+      // 左上方提示
+      helpText: "年份选择器",
+      // 取消按钮
+      cancelText: "取消",
+      // 确认按钮
+      confirmText: "确认",
+
+      // 输入格式错误提示
+      errorFormatText: "输入格式错误",
+      // 输入超出时间范围错误提示
+      errorInvalidText: "输入时间超出范围",
+      // 输入时 - 标签文字
+      fieldLabelText: "请输入时间",
+      // 输入时 - 输入为空 - 提示文字
+      fieldHintText: "时间格式 月/日/年",
+    );
+
+    setState(() {
+      if (_picker != null) {
+        _date = _picker;
+      }
+    });
+  }
+
+  _showTimePickerInput() async {
+    showTimePicker(
+      context: context,
+      // initialTime: TimeOfDay.now(),
+      initialTime: const TimeOfDay(hour: 10, minute: 30),
+      initialEntryMode: TimePickerEntryMode.input,
+      helpText: "时间选择器",
+      confirmText: "确定",
+      cancelText: "取消",
+      hourLabelText: "小时",
+      minuteLabelText: "分钟",
+      errorInvalidText: "错误输入提示",
+    ).then((value) {
+      setState(() {
+        if (value != null) {
+          _times = value;
+          _apm = _times.period.toString() == "DayPeriod.am" ? "上午" : "下午";
+          print(_times.period.toString());
+        }
+        _showTimes = _apm + _times.toString().substring(10, 15);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -128,6 +236,18 @@ class _CalendarDemoState extends State<CalendarDemo> {
           ),
           ElevatedButton(
               onPressed: () {
+                _showDatePickerCalendar();
+              },
+              child: const Text(
+                "showDatePicker 组件 -弹出模式 日历calendar - 路径",
+                // 处理中文错字问题
+                locale: Locale("zh", "CN"),
+              )),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
                 // 是一个方法，不是组件
                 showDatePicker(
                   context: context,
@@ -135,6 +255,9 @@ class _CalendarDemoState extends State<CalendarDemo> {
                   initialDate: DateTime.now(),
                   firstDate: DateTime(2015, 6),
                   lastDate: DateTime(2025, 6),
+
+                  // 弹出模式 - 日历模式
+                  initialEntryMode: DatePickerEntryMode.calendarOnly,
 
                   // 当前时间
                   currentDate: DateTime.now(),
@@ -155,9 +278,128 @@ class _CalendarDemoState extends State<CalendarDemo> {
                   fieldHintText: "时间格式 月/日/年",
                 );
               },
-              child: const Text("showDatePicker 组件")),
+              child: const Text(
+                "showDatePicker 组件 -弹出模式 calendarOnly - 路径",
+                // 处理中文错字问题
+                locale: Locale("zh", "CN"),
+              )),
           const SizedBox(
             height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                _showDatepickeDayInput();
+              },
+              child: const Text(
+                "showDatePicker 组件 -弹出模式 输入 Input - 路径",
+                // 处理中文错字问题
+                locale: Locale("zh", "CN"),
+              )),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                // 是一个方法，不是组件
+                showDatePicker(
+                  context: context,
+                  initialDatePickerMode: DatePickerMode.day,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2015, 6),
+                  lastDate: DateTime(2025, 6),
+
+                  // 弹出模式 - 日历模式
+                  initialEntryMode: DatePickerEntryMode.inputOnly,
+
+                  // 当前时间
+                  currentDate: DateTime.now(),
+                  // 左上方提示
+                  helpText: "年份选择器",
+                  // 取消按钮
+                  cancelText: "取消",
+                  // 确认按钮
+                  confirmText: "确认",
+
+                  // 输入格式错误提示
+                  errorFormatText: "输入格式错误",
+                  // 输入超出时间范围错误提示
+                  errorInvalidText: "输入时间超出范围",
+                  // 输入时 - 标签文字
+                  fieldLabelText: "请输入时间",
+                  // 输入时 - 输入为空 - 提示文字
+                  fieldHintText: "时间格式 月/日/年",
+                );
+              },
+              child: const Text(
+                "showDatePicker 组件 -弹出模式 inputOnly - 路径",
+                // 处理中文错字问题
+                locale: Locale("zh", "CN"),
+              )),
+          const SizedBox(
+            child: Text("############## 获取时间打印 ##############"),
+          ),
+          SizedBox(
+            child: Text(
+              "当前选择的时间是： $_date",
+              style: const TextStyle(color: Colors.red, fontSize: 20),
+            ),
+          ),
+          const SizedBox(
+            child: Text("##############     时钟     ##############"),
+          ),
+          SizedBox(
+            child: Text("当前的时间是：$_showTimes"),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                _showTimePickerInput();
+              },
+              child: const Text("showtimepicker - input")),
+          const SizedBox(
+            child: Text("##############     IOS 风格     ##############"),
+          ),
+          const SizedBox(
+            child: Text(" 时-分  hm  "),
+          ),
+          CupertinoTimerPicker(
+            mode: CupertinoTimerPickerMode.hm,
+            initialTimerDuration: const Duration(hours: 5, minutes: 10),
+            minuteInterval: 10,
+            secondInterval: 10,
+            alignment: Alignment.center,
+            backgroundColor: Colors.blue[100],
+            onTimerDurationChanged: (datTime) {
+              print("选中时间: $datTime");
+            },
+          ),
+          const SizedBox(
+            child: Text(" 分-秒  ms  "),
+          ),
+          CupertinoTimerPicker(
+            mode: CupertinoTimerPickerMode.ms,
+            initialTimerDuration: const Duration(minutes: 10, seconds: 30),
+            minuteInterval: 10,
+            secondInterval: 10,
+            alignment: Alignment.center,
+            backgroundColor: Colors.blue[100],
+            onTimerDurationChanged: (datTime) {
+              print("选中时间: $datTime");
+            },
+          ),
+          const SizedBox(
+            child: Text(" 时-分-秒  hms  "),
+          ),
+          CupertinoTimerPicker(
+            mode: CupertinoTimerPickerMode.hms,
+            initialTimerDuration:
+                const Duration(hours: 5, minutes: 10, seconds: 30),
+            minuteInterval: 10,
+            secondInterval: 10,
+            alignment: Alignment.center,
+            backgroundColor: Colors.blue[100],
+            onTimerDurationChanged: (datTime) {
+              print("选中时间: $datTime");
+            },
           ),
         ],
       ),
